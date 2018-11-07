b821db2f77e54b9d84d9b15862156d1a6ec7c8bc
tekgnu <matthew@digitalanomaly.com>
2018-11-07 13:40:37 -0500

    Draft Code

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title':'Article one',
+			'body':'Article ones super body',
+			'author':'Test Author',
+			'create_date':'10.15.2018'
+		},
+		{
+			'id': 2,
+			'title': 'Article two',
+			'body': 'Article twos super body',
+			'author': 'Test Author',
+			'create_date': '10.16.2018'
+		},
+		{
+			'id': 3,
+			'title': 'Article three',
+			'body': 'Article threes super body',
+			'author': 'Test Author',
+			'create_date': '10.17.2018'
+		}
+	]
+	return articles
